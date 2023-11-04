302581bb14e0d904a0162b954a2ce8feb12443ed
gitbeet <nikig715@gmail.com>
2023-11-04 16:13:32 +0100

    Shopping Bag context with addToBag function

---
+++ src/context/shoppingBagContext.tsx
@@ -0,0 +1,52 @@
+import type { ProductColor, ProductSize } from "@prisma/client";
+import { createContext, useContext, useState } from "react";
+
+const shopingBagContext = createContext<BagContextType | null>(null);
+
+export const useShoppingBagContext = () => {
+  const context = useContext(shopingBagContext);
+  if (!context) {
+    throw new Error("No Shopping Bag context found.");
+  }
+  return context;
+};
+
+type ShoppingBagProduct = {
+  id: string;
+  quantity: number;
+  color: ProductColor;
+  size: ProductSize;
+};
+
+type BagContextType = {
+  shoppingBag: ShoppingBagProduct[];
+  addToBag: (data: ShoppingBagProduct) => void;
+};
+
+const ShoppingBagProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [shoppingBag, setShoppingBag] = useState<ShoppingBagProduct[]>([]);
+
+  const addToBag = (data: ShoppingBagProduct) => {
+    const { color, id, quantity, size } = data;
+    setShoppingBag((prev) => {
+      const itemIndex = prev.findIndex((item) => {
+        item.id === id && item.color === color && item.size === size;
+      });
+      return itemIndex === -1
+        ? [...prev, { id, color, size, quantity }]
+        : prev.map((item) =>
+            item.id === id ? { ...item, quantity: item.quantity + 1 } : item,
+          );
+    });
+  };
+
+  return (
+    <shopingBagContext.Provider value={{ shoppingBag, addToBag }}>
+      {children}
+    </shopingBagContext.Provider>
+  );
+};
+
+export default ShoppingBagProvider;
