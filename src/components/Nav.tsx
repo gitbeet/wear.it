96b6a7c7c10b4dbaf4af2b47ece7c4234573d2d2
gitbeet <nikig715@gmail.com>
2023-11-04 19:08:07 +0100

    nav megamenu bug fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,42 +7,44 @@ const Nav = () => {
   const [type, setType] = useState<"men" | "women" | null>(null);
   const [showMegaMenu, setShowMegaMenu] = useState(false);
   return (
-    <nav className="bg-slate-50">
-      <div className="relative z-50 mx-auto flex max-w-[1400px] items-center justify-between bg-slate-50  ">
-        <h1 className="text-xl font-black">e.fashion</h1>
-        <ul
-          role="navigation"
-          className="flex cursor-pointer  justify-center  shadow-sm"
-        >
-          <NavLink link="/" text="Home" />
-          <NavLink
-            onClick={() => setShowMegaMenu(false)}
-            onMouseOver={() => {
-              setShowMegaMenu(true);
-              setType("men");
-            }}
-            onMouseLeave={() => {
-              setShowMegaMenu(false);
-            }}
-            link="/products/men"
-            text="Men"
-          />
-          <NavLink
-            onClick={() => setShowMegaMenu(false)}
-            onMouseOver={() => {
-              setShowMegaMenu(true);
-              setType("women");
-            }}
-            onMouseLeave={() => {
-              setShowMegaMenu(false);
-            }}
-            link="/products/women"
-            text="Women"
-          />
-          <NavLink link="/products/kids" text="Kids" />
-          <NavLink link="/contact" text="Contact" />
-        </ul>
-        <ShoppingBagIcon />
+    <nav className="bg-slate-50 ">
+      <div className="relative z-50 bg-slate-50  ">
+        <div className=" mx-auto flex max-w-[1400px] items-center justify-between">
+          <h1 className="text-xl font-black">e.fashion</h1>
+          <ul
+            role="navigation"
+            className="flex cursor-pointer  justify-center  shadow-sm"
+          >
+            <NavLink link="/" text="Home" />
+            <NavLink
+              onClick={() => setShowMegaMenu(false)}
+              onMouseOver={() => {
+                setShowMegaMenu(true);
+                setType("men");
+              }}
+              onMouseLeave={() => {
+                setShowMegaMenu(false);
+              }}
+              link="/products/men"
+              text="Men"
+            />
+            <NavLink
+              onClick={() => setShowMegaMenu(false)}
+              onMouseOver={() => {
+                setShowMegaMenu(true);
+                setType("women");
+              }}
+              onMouseLeave={() => {
+                setShowMegaMenu(false);
+              }}
+              link="/products/women"
+              text="Women"
+            />
+            <NavLink link="/products/kids" text="Kids" />
+            <NavLink link="/contact" text="Contact" />
+          </ul>
+          <ShoppingBagIcon />
+        </div>
       </div>
       <MegaMenu type={type} show={showMegaMenu} setShow={setShowMegaMenu} />
     </nav>
