92546d43700231c3f2698800ecf069d7afa45c39
gitbeet <nikig715@gmail.com>
2024-12-06 17:22:16 +0100

    mobile menu close button icon with NavIcon

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { VscChromeClose } from "react-icons/vsc";
 import { useModalsContext } from "~/context/modalsContext";
+import NavIcon from "../Nav/NavIcon";
 
 const MobileMenu = () => {
   const { setShowMobileMenu, showMobileMenu } = useModalsContext();
@@ -12,11 +13,13 @@ const MobileMenu = () => {
           showMobileMenu ? "" : "translate-x-full"
         } fixed bottom-0 right-0 top-0 z-[100] h-screen w-[min(70%,350px)] bg-slate-50 transition-transform duration-500`}
       >
-        <VscChromeClose
-          role="button"
-          className="absolute right-8 top-8 h-6 w-6"
-          onClick={() => setShowMobileMenu(false)}
-        />
+        <div className="absolute right-8 top-8 h-10">
+          <NavIcon
+            as="button"
+            icon={<VscChromeClose role="button" className="h-5 w-5" />}
+            onClick={() => setShowMobileMenu(false)}
+          />
+        </div>
         <div className="h-24"></div>
         <div className="flex flex-col gap-8 pl-12">
           <Link
