a9a8da5597d3e424d6c7665027982af801792f86
gitbeet <nikig715@gmail.com>
2024-12-10 07:51:42 +0100

    webhook improved error handling

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,6 +1,4 @@
-// import Cors from "micro-cors";
 import Stripe from "stripe";
-
 import { PrismaClient } from "@prisma/client";
 import { buffer } from "stream/consumers";
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -8,12 +6,7 @@ const prisma = new PrismaClient();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
-  // apiVersion: "2022-11-15",
 });
-
-// const cors = Cors({
-//   allowMethods: ["POST", "HEAD"],
-// });
 
 export const config = {
   api: {
@@ -21,17 +14,16 @@ export const config = {
   },
 };
 
-const secret = process.env.STRIPE_WEBHOOK_KEY ?? "";
+const secret = process.env.STRIPE_WEBHOOK_KEY;
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
-    console.log("Trying...");
+    if (!secret) throw new Error("No webhook key");
     const buf = await buffer(req);
     const sig = req.headers["stripe-signature"]!;
-    if (!sig) throw new Error("No signature");
+    if (!sig) throw new Error("No stripe signature");
     const event = stripe.webhooks.constructEvent(buf.toString(), sig, secret);
     if (event.type === "payment_intent.succeeded") {
-      console.log("Payment intend has succeeded");
       if (!event.data.object.metadata?.userId) throw new Error("No userId");
       const userId = event.data.object.metadata.userId;
       const session = await prisma.shoppingSession.findUnique({
@@ -49,43 +41,60 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
           },
         },
       });
-
-      console.log("Session fetched:", session);
-
       if (!session) {
         throw new Error("Session for given identifier not found");
       }
-      console.log("Starting transaction...");
-      await prisma.$transaction([
-        prisma.orderDetails.create({
-          data: {
-            userId,
-            total: session.total,
-            orderItems: {
-              createMany: {
-                data: session.cartItems.map(
-                  ({ productId, color, quantity, size }) => ({
-                    productId,
-                    quantity,
-                    color,
-                    size,
-                  }),
-                ),
+      try {
+        await prisma.$transaction([
+          prisma.orderDetails.create({
+            data: {
+              userId,
+              total: session.total,
+              orderItems: {
+                createMany: {
+                  data: session.cartItems.map(
+                    ({ productId, color, quantity, size }) => ({
+                      productId,
+                      quantity,
+                      color,
+                      size,
+                    }),
+                  ),
+                },
               },
             },
-          },
-        }),
-        prisma.shoppingSession.delete({
-          where: {
-            userId,
-          },
-        }),
-      ]);
-      console.log("Transaction completed.");
+          }),
+          prisma.shoppingSession.delete({
+            where: {
+              userId,
+            },
+          }),
+        ]);
+      } catch (prismaTransactionError) {
+        let errorMessage = "Database transaction failed: ";
+        if (prismaTransactionError instanceof Error) {
+          console.error(
+            "Database transaction error: ",
+            prismaTransactionError.message,
+          );
+          errorMessage += prismaTransactionError.message;
+        } else {
+          console.error("Database transaction error: ", prismaTransactionError);
+          errorMessage += "Unknown error";
+        }
+        throw new Error(errorMessage);
+      }
     }
-    res.status(200).send("Success");
+    res.status(200).send("The operation was successful");
   } catch (error) {
-    res.status(400).send(`Webhook Error`);
-    return;
+    const errorResponse = { error: "Webhook processing failed", message: "" };
+    if (error instanceof Error) {
+      console.error("Webhook handler error: ", error.message);
+      errorResponse.message = error.message;
+    } else {
+      console.error("Unexpected error type: ", error);
+      errorResponse.message = "An unknown error has occured";
+    }
+    res.status(400).send(errorResponse);
   }
 }
